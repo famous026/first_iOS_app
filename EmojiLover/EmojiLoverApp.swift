8be569dce72c1f1b1dbdb2fb8d588e3d5831b915
Prakhyat Chaube <prakhyatchaube@gmail.com>
2023-03-06 17:15:25 -0600

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Prakhyat Chaube on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
